4b37655e7f81c1a1e0687a04e486ba3656b1ac9e
Saketh Gunnam <gunnamsrisaketh@gmail.com>
2024-05-16 22:30:47 +0530

    Updated print() and add() methods in Testchild branch

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,5 +1,8 @@
 public class Assignment {
     public String print() {
-        return "This message is from the Test Branch";
+        return "This message is from the TestChild Branch";
+    }
+    public int add(int a , int b) {
+        return a + b;
     }
 }
